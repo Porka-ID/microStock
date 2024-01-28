ac780ccac7518855484832809b504636eeac784f
Porka-ID <kfougere@francoisdesales.be>
2024-01-28 22:36:39 +0100

    New ver

--- cat.py
+++ cat.py
@@ -1,19 +1,31 @@
+from typing import Any
 from db import *
 class ObjectStocked:
-    def __init__(self):
-        pass
+    def __init__(self, name, type, qty, infos):
+        self.data = DatabaseItem()
+        self.name = name
+        self.type = type
+        self.qty = qty
+        self.infos = infos
 
     def addToStock(self):
-        pass
+        if self.name and self.qty and self.infos:
+            self.data.insertStock({"name": self.name, "type": self.type, "qty": self.qty, "infos": self.infos})
 
 
 class Drink(ObjectStocked):
-    def __init__(self):
-        #super().__init__()
-        pass
+    def __init__(self, name, qty, infos):
+        super().__init__(name, "Drink", qty, infos)
+
+    def 
+
+        
 
 
 class Food(ObjectStocked):
-    def __init__(self):
-        #super().__init__()
-        pass
+    def __init__(self, name, qty, infos):
+        super().__init__(name, "Food", qty, infos)
+        
+
+if __name__ == "__main__":
+    newF = Food("Chicken", 1, {"Nutriment": "100ml de "}).addToStock()
--- db.py
+++ db.py
@@ -15,14 +15,48 @@ class DatabaseItem(Database):
         self.docs = self.col.find(self.query)
         try:
             self.docs = self.docs[0]
-            print(self.docs)
+               
+        except:
+            self.docs = None
+        
+
+    def updateStock(self, tbl):
+        tbl["qty"] = self.docs["qty"] + tbl["qty"]
+        self.docs = tbl
+        try:
+            self.newLine = {"$set": self.docs}
+            self.col.update_one({"name": tbl["name"]}, self.newLine )
+            self.getItemStock(tbl["name"])
+            print(tbl)
+            if int(self.docs["qty"]) < 0:
+                print("Vous tentez de supprimer du stock qui n'existe pas !")
+            self.docs = None
+            return
+            
         except Exception as e:
-            print(e)
+            print(e)   
+            
+    def deleteStock(self, tbl):
+        if tbl:
+            self.col.delete_one({"name": tbl["name"]})
 
 
     def insertStock(self, tbl):
-        pass
+        if tbl and tbl["name"]:
+            self.getItemStock(tbl["name"])
+            if not self.docs:
+                try:
+                    self.col.insert_one(tbl)
+                except Exception as e:
+                    print(e)
+            else:
+                self.updateStock(tbl)
+                
+    
+    def removeStock(self, tbl):
+        tbl["qty"] = -tbl["qty"]
+        self.insertStock(tbl)
+
 
 if __name__ ==  "__main__":
-    newDB = DatabaseItem()
-    newDB.getItemStock("Ice-Tea")
+    pass
